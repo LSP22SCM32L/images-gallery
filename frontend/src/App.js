5c2bc052ba7efc1b4a260c32becd045826acedd1
mercurie <lucas.lefrancois16@gmail.com>
2022-07-28 17:01:24 -0500

    display imagecards and functional delete button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 //we dont have to use curly braces for Header because it was exported with default
 import Search from './components/Search';
 import { useState } from 'react';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -16,22 +18,24 @@ function App() {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images);
-
   function handleSearchSubmit(e) {
     e.preventDefault();
-    console.log(word); //access the inputted value when search is pressed
+    // console.log(word); //access the inputted value when search is pressed
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
       });
     setWord('');
+  }
+
+  function handleDeleteImage(id) {
+    setImages(images.filter((image) => image.id !== id));
   }
 
   // console.log(process.env);
@@ -42,6 +46,15 @@ function App() {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, index) => (
+            <Col key={index} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   ); //THIS IS JSX SYNTAX
 }
