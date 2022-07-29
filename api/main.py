113d0f23a3ce00bf8a1434434be55feb06a55357
mercurie <lucas.lefrancois16@gmail.com>
2022-07-29 16:22:37 -0500

    new request for unsplash

--- api/main.py
+++ api/main.py
@@ -1,13 +1,37 @@
-from flask import Flask
+import os
+import requests
+#this requests module will be used to CREATE client 
+# requests and send them to ANOTHER server. THIS IS SEPARATE FROM FLASK REQUEST
+from flask import Flask, request
+from dotenv import load_dotenv
 
-# print(__name__)
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL='https://api.unsplash.com/photos/random'
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError(".env.local needed with key")
 
 app = Flask(__name__)
 #__name__ is set at runtime and is the name of the module
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    #this request objct is from FLASK which is used to access client 
+    # requests that is sent TO THE Flask application from the OTHER clients
+    
+    headers = {
+        "Authorization": f"Client-ID {UNSPLASH_KEY}",
+        "Accept-Version": "v1"
+    }
+    params = {
+        "query": word
+    }
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
