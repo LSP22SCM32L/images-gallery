9659e24b056ac47814b40f8efbf0b97cee3bcf89
mercurie <lucas.lefrancois16@gmail.com>
2022-07-29 17:14:03 -0500

    enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import os
 import requests
-#this requests module will be used to CREATE client 
+
+# this requests module will be used to CREATE client
 # requests and send them to ANOTHER server. THIS IS SEPARATE FROM FLASK REQUEST
 from flask import Flask, request
 from dotenv import load_dotenv
@@ -8,35 +9,32 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL='https://api.unsplash.com/photos/random'
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError(".env.local needed with key")
 
 app = Flask(__name__)
-#__name__ is set at runtime and is the name of the module
+# __name__ is set at runtime and is the name of the module
 CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    #this request objct is from FLASK which is used to access client 
+    # this request objct is from FLASK which is used to access client
     # requests that is sent TO THE Flask application from the OTHER clients
-    
-    headers = {
-        "Authorization": f"Client-ID {UNSPLASH_KEY}",
-        "Accept-Version": "v1"
-    }
-    params = {
-        "query": word
-    }
+
+    headers = {"Authorization": f"Client-ID {UNSPLASH_KEY}", "Accept-Version": "v1"}
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
